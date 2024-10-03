93f4b72a8b7b69e0ffdb131a5e60e15b920bf330
Alejaddro Guevara <alejandroguevaranavas2@gmail.com>
2024-10-03 00:01:30 -0500

    Se creo la carpeta llamada ElChoco, para implementar la app Android

--- IntegracionElChocontano/app/+not-found.tsx
+++ IntegracionElChocontano/app/+not-found.tsx
@@ -9,9 +9,9 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
+        <ThemedText type="title">ESTA PAGINA NO EXISTE BESTIA</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">¡VE AL INICIO!</ThemedText>
         </Link>
       </ThemedView>
     </>
